59c088131a719b016b82554886faea8ffb0800a8
Kazi Islam <kazichaska@gmail.com>
2022-03-27 12:28:56 -0500

    offline functionality work using indexedDB

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -67,7 +67,8 @@ const handlePizzaSubmit = event => {
     })
     .catch(err => {
       console.log(err);
-    })
+      saveRecord(formData);
+    });
 };
 
 $pizzaForm.addEventListener('submit', handlePizzaSubmit);
