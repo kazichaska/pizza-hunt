2bc9d3b24575b46de7174abec37d1b7ae762fe22
Kazi Islam <kazichaska@gmail.com>
2022-03-27 21:29:51 -0500

    changes on server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
-    useFindAndModify: false,
+    // useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
