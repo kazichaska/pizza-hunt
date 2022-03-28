536c8ed708ff24453360246f6a74f015810b01ee
Kazi Islam <kazichaska@gmail.com>
2022-03-27 19:32:23 -0500

    updated server.js because of heroku app crash

--- server.js
+++ server.js
@@ -8,14 +8,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('./routes'));
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 // Use this to log mongo queries beging executed!
 mongoose.set('debug', true);
+
+app.use(require('./routes'));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
